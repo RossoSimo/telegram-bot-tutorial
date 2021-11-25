164770306243cc89cff4cb2ad33f4ce8511ebc23
Simone Rosso <simone.rosso@itiszuccante.edu.it>
2021-11-25 18:28:06 +0100

    Add esempio tastiera

--- Tutorial_BotTelegram/Example/simpleKeyboard_bot.py
+++ Tutorial_BotTelegram/Example/simpleKeyboard_bot.py
@@ -1,32 +1,46 @@
-import time
 import telepot
 from telepot.loop import MessageLoop
 from telepot.namedtuple import InlineKeyboardMarkup, InlineKeyboardButton
+ 
+from pprint import pprint
+import time
+import datetime
+import json
+from urllib.request import urlopen
+ 
 
-TOKEN = "1987567608:AAHER8qfBeqMONykRMqkBYmLJ1IuwBWX_3Y"
-
-def handle(msg):    #Funzione richiamata nel momento in cui gli si invia un messaggio
-    content_type, chat_type, chat_id = telepot.glance(msg)
-    testo = msg['text'] #Assegnamo alla variabile testo il testo del messaggio inviato
-    username = msg['from']['username']  #Assegnamo alla variabile username l'username dell'utente che invia il messaggio
-    if testo == '/start':
-        keyboard =InlineKeyboardMarkup(inline_keyboard=[
-                   [InlineKeyboardButton(text='Press me', callback_data='press')],
-               ])
-        bot.sendMessage(chat_id, "Benvenuto nel bot!", reply_markup=keyboard)
-    else:
-        bot.sendMessage(chat_id, "Non capisco")
-
+TOKEN="INSERISCI TOKEN QUI" # da cambiare
+ 
+def on_chat_message(msg):
+	content_type, chat_type, chat_id = telepot.glance(msg)
+ 
+	keyboard = InlineKeyboardMarkup(inline_keyboard=[
+                     [InlineKeyboardButton(text='IP', callback_data='ip'),
+                     InlineKeyboardButton(text='Info', callback_data='info')],
+                     [InlineKeyboardButton(text='Time', callback_data='time')],
+                 ])
+	bot.sendMessage(chat_id, 'Usa la tastiera qui sotto', reply_markup=keyboard)
+ 
+ 
 def on_callback_query(msg):
-    query_id, from_id, query_data = telepot.glance(msg, flavor='callback_query')
-    print('Callback Query:', query_id, from_id, query_data)
-
-    bot.answerCallbackQuery(query_id, text='Got it')
-
-bot = telepot.Bot(TOKEN)    #Creiamo l'oggetto bot, con il nostro token precedentemente dichiarato
-MessageLoop(bot, {'chat': handle,
-                  'callback_query': on_callback_query}).run_as_thread()    #Mettiamo il bot in ascolto dei messaggi o delle azioni nella tastiera in arrivo
+	query_id, chat_id, query_data = telepot.glance(msg, flavor='callback_query')
+	print('Callback Query:', query_id, chat_id, query_data)
+	if query_data=='ip': # Fa una richiesta web per ricavare l'ip
+		my_ip = urlopen('http://ip.42.pl/raw').read()
+		bot.sendMessage(chat_id, my_ip)
+	elif query_data=='info': # invia info del utente
+		info=json.dumps(bot.getUpdates(),sort_keys=True, indent=4)
+		bot.sendMessage(chat_id, info)
+	elif query_data=='time': # Invia il tempo
+		ts = time.time()
+		bot.answerCallbackQuery(query_id, text=datetime.datetime.fromtimestamp(ts).strftime('%H:%M:%S')) #messaggio a comparsa
+ 
+ 
+bot = telepot.Bot(TOKEN)
+MessageLoop(bot, {'chat': on_chat_message,
+				  'callback_query': on_callback_query}).run_as_thread() 
 print('Listening ...')
-
-while 1: #Manteniamo il programma in esecuzione
-    time.sleep(10)
+ 
+ 
+while 1:
+	time.sleep(10)
--- Tutorial_BotTelegram/README.md
+++ Tutorial_BotTelegram/README.md
@@ -1,5 +1,6 @@
 # Tutorial Bot telegram in Python
-## Primo esempio:
+
+## [Primo esempio](Example\echo_bot.py):
 Questo bot ripeterá il messaggio che gli si invia, se il messaggio é di tipo testo.
 ```python
 import time
@@ -25,7 +26,7 @@ while 1: #Manteniamo il programma in esecuzione
     time.sleep(10)
 ```
 
-## Secondo esempio:
+## [Secondo esempio](Example\chat_bot.py):
 ```python
 import time
 import telepot
@@ -54,4 +55,55 @@ print('Listening ...')
 
 while 1: #Manteniamo il programma in esecuzione
     time.sleep(10)
+```
+
+## [Terzo esempio](Example/simpleKeyboard_bot.py):
+Crea una tastiera a schermo dove poter scegliere varie opzioni.
+```python
+import telepot
+from telepot.loop import MessageLoop
+from telepot.namedtuple import InlineKeyboardMarkup, InlineKeyboardButton
+ 
+from pprint import pprint
+import time
+import datetime
+import json
+from urllib.request import urlopen
+ 
+
+TOKEN="INSERISCI TOKEN QUI" # da cambiare
+ 
+def on_chat_message(msg):
+	content_type, chat_type, chat_id = telepot.glance(msg)
+ 
+	keyboard = InlineKeyboardMarkup(inline_keyboard=[
+                     [InlineKeyboardButton(text='IP', callback_data='ip'),
+                     InlineKeyboardButton(text='Info', callback_data='info')],
+                     [InlineKeyboardButton(text='Time', callback_data='time')],
+                 ])
+	bot.sendMessage(chat_id, 'Usa la tastiera qui sotto', reply_markup=keyboard)
+ 
+ 
+def on_callback_query(msg):
+	query_id, chat_id, query_data = telepot.glance(msg, flavor='callback_query')
+	print('Callback Query:', query_id, chat_id, query_data)
+	if query_data=='ip': # Fa una richiesta web per ricavare l'ip
+		my_ip = urlopen('http://ip.42.pl/raw').read()
+		bot.sendMessage(chat_id, my_ip)
+	elif query_data=='info': # invia info del utente
+		info=json.dumps(bot.getUpdates(),sort_keys=True, indent=4)
+		bot.sendMessage(chat_id, info)
+	elif query_data=='time': # Invia il tempo
+		ts = time.time()
+		bot.answerCallbackQuery(query_id, text=datetime.datetime.fromtimestamp(ts).strftime('%H:%M:%S')) #messaggio a comparsa
+ 
+ 
+bot = telepot.Bot(TOKEN)
+MessageLoop(bot, {'chat': on_chat_message,
+				  'callback_query': on_callback_query}).run_as_thread() 
+print('Listening ...')
+ 
+ 
+while 1:
+	time.sleep(10)
 ```
